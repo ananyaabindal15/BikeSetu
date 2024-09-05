1b87660b09a09cdb8138cedc0699c4aea44e8513
ananyaabindal <131257473+ananyaabindal15@users.noreply.github.com>
2024-09-06 02:13:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let offers = document.querySelector(".offers")
+let aboutoffers = document.querySelector(".tickets")
+offers.addEventListener('click', function() {
+    aboutoffers.scrollIntoView({ behavior: 'smooth' });
+});
+let about = document.querySelector(".aboutus")
+let aboutinfo = document.querySelector(".about")
+about.addEventListener('click', function() {
+    aboutinfo.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
